0beb41045baa2d129b0cf96ed4e4da399eafca8c
Sergey <Mordorot@yandex.ru>
2017-04-11 12:59:40 +0700

    Commit 3
    Открытие папки

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 
+#include <windows.h>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -11,7 +12,25 @@ using namespace std;
 
 class Cypher
 {
+public:
 	
+	void OpenFolder(string path)
+	{
+		WIN32_FIND_DATA FindFileData;
+		HANDLE hf;
+		string res = "c:\\" + path + "\\*";
+
+		hf = FindFirstFile(res.c_str(), &FindFileData);
+
+		if (hf != INVALID_HANDLE_VALUE) 
+		{
+			do 
+			{
+				std::cout << FindFileData.cFileName << "\n";
+			} while (FindNextFile(hf, &FindFileData) != 0);
+			FindClose(hf);
+		}
+	}
 
 };
 
@@ -21,10 +40,22 @@ TEST(Test_DDT_Pile, Test_Open_Object)
 	ASSERT_NO_FATAL_FAILURE(cyp);
 }
 
+TEST(Test_DDT_Pile, Test_Open_Folder)
+{
+	Cypher cyp;
+	ASSERT_NO_FATAL_FAILURE(cyp.OpenFolder("TDD_Cypher"));
+}
+
 int main(int argc, char* argv[])
 {
+	
+
 	::testing::InitGoogleTest(&argc, argv);
 	RUN_ALL_TESTS();
+
+	
+
 	system("pause");
+	
 	return 0;
 }
