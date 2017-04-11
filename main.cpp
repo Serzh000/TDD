87344cee8c61bbd9007440cc61848ae2ff22b798
Sergey <Mordorot@yandex.ru>
2017-04-11 15:48:55 +0700

    Commit 18
    Дешифровка проходит все тесты после рефакторинга.

--- main.cpp
+++ main.cpp
@@ -38,7 +38,8 @@ public:
 	{
 		string result = "";
 
-		shift %= 26;
+		if (shift>26)
+			shift %= 26;
 		
 		for (int i = 0; i < str.length(); i++)
 		{
@@ -78,13 +79,37 @@ public:
 	{
 		string result = "";
 
-		shift %= 26;
+		if(shift>26)
+			shift %= 26;
 
-			for (int i = 0; i < str.length(); i++)
+		for (int i = 0; i < str.length(); i++)
+		{
+			if (((int)(str[i]) < 65) || ((int)(str[i]) > 122))
+				result += str[i];
+
+			if (((int)(str[i]) >= 97) && ((int)(str[i]) <= 122))
 			{
-					
-				result += (char)((int)(str[i]) - shift);
+
+				if ((int)(str[i]) - shift < 97)
+
+					result += (char)((int)(str[i]) - shift + 26);
+
+				else
+
+					result += (char)((int)(str[i]) - shift);
 			}
+
+			if (((int)(str[i]) >= 65) && ((int)(str[i]) <= 90))
+			{
+				if ((int)(str[i]) - shift < 65)
+
+					result += (char)((int)(str[i]) - shift + 26);
+
+				else
+
+					result += (char)((int)(str[i]) - shift);
+			}
+		}
 			
 			return result;
 	}
