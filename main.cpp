714e4759c1cf6c76448f9d8dbe9040cab48d07fe
Sergey <Mordorot@yandex.ru>
2017-04-11 12:13:19 +0700

    Create main.cpp
    Первый коммит

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include "gtest/gtest.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+
+class TestClass
+{
+
+};
+
+
+class Cypher
+{
+
+};
+
+
+int main(int argc, char* argv[])
+{
+	::testing::InitGoogleTest(&argc, argv);
+	RUN_ALL_TESTS();
+	system("pause");
+	return 0;
+}
